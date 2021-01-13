a6342408da8b97fc1c06698d5a9272f5a673951c
Apollinaire <no-reply@apollinai.re>
2021-01-13 12:00:47 +0100

    restore manifest content script

--- src/background.js
+++ src/background.js
@@ -1,8 +1,14 @@
-chrome.webNavigation.onCommitted.addListener(function (o) {
-  console.log('inejct');
-  chrome.tabs.executeScript(o.tabId, {
-    file: 'content.bundle.js',
-    runAt: 'document_start',
-    allFrames: true,
-  });
+console.log('start background');
+
+chrome.webNavigation.onCommitted.addListener(function (event) {
+  console.log('onCommited', event);
+  const url = new URL(event.url);
+  if (url.host.endsWith('mediapart.fr')) {
+    try {
+      console.log('send applyStyles');
+      chrome.tabs.sendMessage(event.tabId, { method: 'applyStyles' });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 });
--- src/injectTheme.js
+++ src/injectTheme.js
@@ -1,6 +1,7 @@
 import newCSS from './generateCSS/newMediapart.css';
+console.log('start extension', Date.now());
 
-function run() {
+function applyStyles() {
   const cssPrefix = `
     /*
       THEME Start 
@@ -27,4 +28,14 @@ body {
   document.head.append(styleEl);
 }
 
-run();
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(message, sender);
+  switch (message.method) {
+    case 'applyStyles':
+      applyStyles();
+      sendResponse({ method: 'applyStyles', done: true });
+      break;
+    default:
+      console.log('unhandled message method: ', message.method, message);
+  }
+});
