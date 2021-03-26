39ed97803b52a879a2241eca5f4753d1936a4a0b
Apollinaire <no-reply@apollinai.re>
2021-03-26 23:33:05 +0100

    cap lightness

--- src/generateCSS/colorUtils/invertRGBColor.ts
+++ src/generateCSS/colorUtils/invertRGBColor.ts
@@ -1,10 +1,20 @@
 import convert from 'color-convert';
 import { RGB } from 'color-convert/conversions';
 
-
 export type ColorType = 'background' | 'text';
 
-export const invertRGBColor = ([ r, g, b ]: RGB, colorType: ColorType): RGB => {
+export const invertRGBColor = ([r, g, b]: RGB, colorType: ColorType): RGB => {
   const [h, s, l] = convert.rgb.hsl(r, g, b);
-  return convert.hsl.rgb([h, s, 100 - l]);
+  return convert.hsl.rgb([h, s, invertLightness(l, colorType)]);
+};
+
+const invertLightness = (lightness: number, colorType: ColorType) => {
+  switch (colorType) {
+    case 'background':
+      return Math.max(100 - lightness, 7.1);
+    case 'text':
+      return Math.min(100 - lightness, 93);
+    default:
+      return lightness;
+  }
 };
