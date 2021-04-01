3135805bb9071202b117cc6d6e8bc0c614ded2d4
Apollinaire <no-reply@apollinai.re>
2021-04-01 21:57:56 +0200

    build 0.0.1

--- dist/raw/background.bundle.js
+++ dist/raw/background.bundle.js
@@ -1,1 +1,107 @@
-(()=>{"use strict";const e=["zenMode","darkTheme"],o={zenMode:!0,darkTheme:!0},t=()=>new Promise((t=>{chrome.storage.sync.get(e,(e=>{t({...o,...e})}))})),s=e=>{chrome.tabs.sendMessage(e,{method:"applyStyles"})},a=()=>{chrome.tabs.query({},((e=[])=>{e.forEach((e=>{s(e.id)}))}))};console.log("start background"),chrome.runtime.onMessage.addListener(((e,o,t)=>{const{method:s}=e;switch(s){case"getStyles":return void t({done:!0})}})),chrome.webNavigation.onCommitted.addListener((function(e){if(console.log("onCommited",e),new URL(e.url).host.endsWith("mediapart.fr"))try{console.log("send applyStyles"),s(e.tabId)}catch(e){console.log(e)}})),chrome.commands.onCommand.addListener((async e=>{switch(e){case"toggle-theme":const{darkTheme:n}=await t();console.log({command:e,darkTheme:n}),await(s=!n,new Promise((e=>{chrome.storage.sync.set({darkTheme:s},e)}))),a();break;case"toggle-zen":const{zenMode:r}=await t();await(o=!r,new Promise((e=>{chrome.storage.sync.set({zenMode:o},e)}))),a()}var o,s}))})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+;// CONCATENATED MODULE: ./src/utils/config.js
+const CONFIG_KEYS = ['zenMode', 'darkTheme'];
+const DEFAULT_CONFIG = {
+  zenMode: true,
+  darkTheme: true
+};
+const getConfig = () => {
+  return new Promise(resolve => {
+    chrome.storage.sync.get(CONFIG_KEYS, config => {
+      resolve({ ...DEFAULT_CONFIG,
+        ...config
+      });
+    });
+  });
+};
+const setZenMode = newZenMode => {
+  return new Promise(resolve => {
+    chrome.storage.sync.set({
+      zenMode: newZenMode
+    }, resolve);
+  });
+};
+const setDarkTheme = newDarkTheme => {
+  return new Promise(resolve => {
+    chrome.storage.sync.set({
+      darkTheme: newDarkTheme
+    }, resolve);
+  });
+};
+;// CONCATENATED MODULE: ./src/utils/messages.js
+const sendApplyStyles = tabId => {
+  chrome.tabs.sendMessage(tabId, {
+    method: 'applyStyles'
+  });
+};
+const sendApplyStylesToAll = () => {
+  chrome.tabs.query({}, (tabs = []) => {
+    tabs.forEach(tab => {
+      sendApplyStyles(tab.id);
+    });
+  });
+};
+;// CONCATENATED MODULE: ./src/background.js
+
+
+console.log('start background');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  const {
+    method
+  } = message;
+
+  switch (method) {
+    case 'getStyles':
+      // todo get config from localhost and return it
+      sendResponse({
+        done: true
+      });
+      return;
+
+    default:
+      break;
+  }
+});
+chrome.webNavigation.onCommitted.addListener(function (event) {
+  console.log('onCommited', event);
+  const url = new URL(event.url);
+
+  if (url.host.endsWith('mediapart.fr')) {
+    try {
+      console.log('send applyStyles');
+      sendApplyStyles(event.tabId);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+chrome.commands.onCommand.addListener(async command => {
+  switch (command) {
+    case 'toggle-theme':
+      const {
+        darkTheme
+      } = await getConfig();
+      console.log({
+        command,
+        darkTheme
+      });
+      await setDarkTheme(!darkTheme);
+      sendApplyStylesToAll();
+      break;
+
+    case 'toggle-zen':
+      const {
+        zenMode
+      } = await getConfig();
+      await setZenMode(!zenMode);
+      sendApplyStylesToAll();
+      break;
+
+    default:
+      break;
+  }
+});
+/******/ })()
+;
