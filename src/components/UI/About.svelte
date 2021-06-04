561ab43c3ca84b102691b308c4a74ba0ce2e98b1
Apollinaire <no-reply@apollinai.re>
2021-06-04 16:33:40 +0200

    feat(options): wording

--- src/components/UI/About.svelte
+++ src/components/UI/About.svelte
@@ -1,7 +1,11 @@
 <p>
-  Mediapart Mod a été imaginée, créée et programmée par <a href="https://www.apollinai.re">Apollinaire</a>. L'ensembe du
-  code est libre et open-source: il est public, gratuit, toute modification, publication ou vente sont permis sans
-  contreparties.
+  L'extension Mediapart Mod a été imaginée, créée et programmée par <a href="https://www.apollinai.re">Apollinaire</a>.
+  L'ensembe du code est libre et open-source: il est public, gratuit, toute modification, publication ou vente sont
+  permis sans contreparties.
+</p>
+<p>
+  Pour tout retour, toute suggestion ou contribution, postez une <i>issue</i> sur le repo Github du projet:
+  <a href="https://github.com/apollinaire/mediapart-mod">apollinaire/mediapart-mod</a>
 </p>
 <p>Librairies externes utilisées, mentions notables:</p>
 <ul>
