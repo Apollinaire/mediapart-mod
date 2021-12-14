6b689f7db32713d475a2be176ae427c66fb1f239
Apollinaire <no-reply@apollinai.re>
2021-12-14 20:08:17 +0100

    feat(dark): add missing css file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,4 +9,5 @@ export const cssLinks = [
   'https://www.mediapart.fr/assets/front/css/studio.min.css',
   'https://www.mediapart.fr/assets/front/nouvelle_formule/css/cc.min.css',
   'https://www.mediapart.fr/assets/front/nouvelle_formule/css/old_formula_nav.min.css',
+  'https://blogs.mediapart.fr/assets/front/js/tinymce/skins/ui/oxide/skin.min.css',
 ];
