276feccbd5c42447a32addab6accb96fcbf91855
Apollinaire <no-reply@apollinai.re>
2022-07-01 20:53:18 +0200

    fix(dark): add missing css files

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,15 +1,20 @@
 export const cssLinks = [
   'https://www.mediapart.fr/assets/front/css/main.min.css',
+  'https://www.mediapart.fr/assets/build/default/front/css/main.min.css',
   'https://blogs.mediapart.fr/assets/front/nouvelle_formule/css/main.min.css',
+  'https://www.mediapart.fr/assets/build/default/front/nouvelle_formule/css/main.min.css',
   'https://blogs.mediapart.fr/assets/front/css/main.min.css',
   'https://blogs.mediapart.fr/assets/front/nouvelle_formule/css/cc.min.css',
+  'https://www.mediapart.fr/assets/build/default/front/nouvelle_formule/css/cc.min.css',
   'https://blogs.mediapart.fr/assets/front/nouvelle_formule/css/old_formula_nav.min.css',
   'https://www.mediapart.fr/assets/front/css/landing_mobile.min.css',
   'https://www.mediapart.fr/assets/front/nouvelle_formule/css/main.min.css',
   'https://www.mediapart.fr/assets/front/css/studio.min.css',
+  'https://www.mediapart.fr/assets/build/default/front/css/studio.min.css',
   'https://www.mediapart.fr/assets/front/nouvelle_formule/css/cc.min.css',
   'https://www.mediapart.fr/assets/front/nouvelle_formule/css/old_formula_nav.min.css',
   'https://blogs.mediapart.fr/assets/front/js/tinymce/skins/ui/oxide/skin.min.css',
   'https://www.mediapart.fr/assets/front/css/account.min.css',
-  'https://www.mediapart.fr/assets/front/nouvelle_formule/css/backport.min.css'
+  'https://www.mediapart.fr/assets/front/nouvelle_formule/css/backport.min.css',
+  'https://www.mediapart.fr/assets/build/default/front/nouvelle_formule/css/backport.min.css',
 ];
